636893c0b07de12e5af0f5c2780bc26f38410c7c
Leonardo Batistuzzo <leonaardobm@gmail.com>
2023-09-27 15:48:21 -0300

    Creating authContext

--- Miniblog/src/App.jsx
+++ Miniblog/src/App.jsx
@@ -1,31 +1,33 @@
-import './App.css'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-
-
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path= "/" element={<Home />} />
-            <Route path= "/about" element={<About />} />
-            <Route path= "/login" element={<Login />} />
-            <Route path= "/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+    <div className="App">
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
