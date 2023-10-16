47e72f7c61e1c974bf95d8b2b3a7fc59fa0821fe
Leonardo Batistuzzo <leonaardobm@gmail.com>
2023-10-16 20:05:44 -0300

    Showing post

--- Miniblog/src/pages/Home/Home.jsx
+++ Miniblog/src/pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import styles from "./Home.module.css";
 
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import PostDetail from "../../components/PostDetail";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([])
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,11 +25,14 @@ const Home = () => {
         <button className="btn btn-dark">Pesquisar</button>
       </form>
       <div>
-        <h1>Posts</h1>
-        {posts && posts.length === 0 &&(
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+        {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
-            <Link to="posts/creaate" className="btn">Criar primeiro post</Link>
+            <Link to="posts/creaate" className="btn">
+              Criar primeiro post
+            </Link>
           </div>
         )}
       </div>
